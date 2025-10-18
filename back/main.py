fabcfb85859ea0f984e69c9a48607bcb94e8667d
Tema Iakovlev <gorsokziv0@gmail.com>
2025-10-18 20:48:42 +0300

    normalize query

--- back/main.py
+++ back/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import router
 import uvicorn
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # домены фронта
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 @app.get("/")
 async def root():
