4b821b3a0957004179e85b887b6a5232bfc9c2c3
Tema <gorsokziv0@gmail.com>
2025-10-19 10:06:19 +0300

    Add logging configuration setup
    Initialize logging configuration with file and stream handlers.

---
+++ tg/logging_config.py
@@ -0,0 +1,18 @@
+import logging  # библиотека для сохранения логов ошибок в файл
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(os.getenv('LOG_FILE')),
+            logging.StreamHandler()
+        ]
+    )
+
+
+logger = logging.getLogger(__name__)
